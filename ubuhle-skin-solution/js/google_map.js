675a441400fb66d6ff0a2206e957a688bacc2047
Phumlani Mhlungu <74601957+mhlunguep@users.noreply.github.com>
2021-11-30 19:29:55 +0000

    Add files

---
+++ ubuhle-skin-solution/js/google_map.js
@@ -0,0 +1,111 @@
+ // Initialize and add the map
+ function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -29.8157, lng: 31.0257 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: uluru,
+        styles: [  //optional styles - Dark night
+            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+            { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+            {
+                featureType: "administrative.locality",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "poi",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "poi.park",
+                elementType: "geometry",
+                stylers: [{ color: "#263c3f" }],
+            },
+            {
+                featureType: "poi.park",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#6b9a76" }],
+            },
+            {
+                featureType: "road",
+                elementType: "geometry",
+                stylers: [{ color: "#38414e" }],
+            },
+            {
+                featureType: "road",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#212a37" }],
+            },
+            {
+                featureType: "road",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#9ca5b3" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry",
+                stylers: [{ color: "#746855" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "geometry.stroke",
+                stylers: [{ color: "#1f2835" }],
+            },
+            {
+                featureType: "road.highway",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#f3d19c" }],
+            },
+            {
+                featureType: "transit",
+                elementType: "geometry",
+                stylers: [{ color: "#2f3948" }],
+            },
+            {
+                featureType: "transit.station",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#d59563" }],
+            },
+            {
+                featureType: "water",
+                elementType: "geometry",
+                stylers: [{ color: "#17263c" }],
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.fill",
+                stylers: [{ color: "#515c6d" }],
+            },
+            {
+                featureType: "water",
+                elementType: "labels.text.stroke",
+                stylers: [{ color: "#17263c" }],
+            },
+        ],
+    });
+    // ....The marker, positioned at Uluru .....
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        icon:  'googlemapicon.png'                    //to add your custom icons
+    });
+
+    //...info window.... 
+    const infowindow = new google.maps.InfoWindow({
+        content: '<h1>Phumlani Mhlungu</h1> this is where you find me',
+    });
+    //... add an event listener to the marker ....
+    marker.addListener("click", () => {
+        infowindow.open({
+        anchor: marker,
+        map,
+        shouldFocus: true,
+        });
+    });
+
+    //....to add more than one maker, create a function
+}
